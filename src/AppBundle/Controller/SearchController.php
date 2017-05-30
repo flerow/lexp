f7079d030b24823cbd52c9cb2b3ad9852e99c8ec
flerow <maria.nedzinska@gmai.com>
2017-05-30 09:31:15 +0200

    Seraching in users added

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -32,7 +32,7 @@ class SearchController extends Controller
     }
 
     /**
-     * @Route("show/{localization}/{tags}")
+     * @Route("/show/{localization}/{tags}")
      * @Template("AppBundle:User:showResearches.html.twig")
      */
     public function showFoundAction($localization, $tags)
@@ -52,5 +52,52 @@ class SearchController extends Controller
             $researches = $this->getDoctrine()->getRepository('AppBundle:Research')->findAll();
         }
         return ['researches' => $researches];
+    }
+
+
+    /**
+     * @Method("POST")
+     * @Route("/genUs")
+     */
+    public function setPathUsAction(Request $request)
+    {
+        $localization = $request->request->get('localization', 'all');
+        $tags = $request->request->get('tags');
+        if (!$tags) {
+            $tags = 'all';
+        }
+        if (!$localization) {
+            $localization = 'all';
+        }
+
+        return $this->redirectToRoute('app_search_showfoundus', ['localization' => $localization, 'tags' => $tags]);
+    }
+
+
+    /**
+     * @Route("/show/users/{localization}/{tags}")
+     * @Template("AppBundle:Manager:showUsers.html.twig")
+     */
+    public function showFoundUsAction($localization, $tags)
+    {
+        if (!($localization == 'all') && !($tags == 'all')) {
+            $tags = explode(',', $tags);
+            foreach ($tags as $i => $tag) {
+                $tags[$i] = trim($tag);
+            }
+            $query = "SELECT u FROM AppBundle:User u JOIN u.tags t WHERE t.name IN (:tags) AND u.localization = :localization";
+            $users = $this->getDoctrine()->getEntityManager()
+                ->createQuery($query)->setParameter('localization', $localization)->setParameter('tags', $tags)->getResult();
+
+        } else if (!($localization == 'all')) {
+            $users = $this->getDoctrine()->getRepository('AppBundle:User')->findBy(['localization' => $localization]);
+        } else if (!($tags == 'all')) {
+            $query = "SELECT u FROM AppBundle:User u JOIN u.tags t WHERE t.name IN (:tags)";
+            $users = $this->getDoctrine()->getEntityManager()
+                ->createQuery($query)->setParameter('tags', $tags)->getResult();
+        } else {
+            $users = $this->getDoctrine()->getRepository('AppBundle:User')->findAll();
+        }
+        return ['users' => $users];
     }
 }
